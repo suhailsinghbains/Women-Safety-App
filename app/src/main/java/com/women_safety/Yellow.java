1be83c5f7ea000d786a56f2cb21520dfa811af13
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-01-12 21:29:27 +0530

    Initial Commit

---
+++ app/src/main/java/com/women_safety/Yellow.java
@@ -0,0 +1,14 @@
+package com.women_safety;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Yellow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yellow);
+    }
+    //private class MyAsyncTask<>
+}
